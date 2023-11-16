8386fb98cab28a21b080a098ef30b5e0a6b1e814
KyawZinLin3 <kyawzinlin14future@gmail.com>
2023-11-17 00:07:56 +0630

    Add project files.

---
+++ BookListRazor/Migrations/20231115054908_AddISBNToBookModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookListRazor.Migrations
+{
+    public partial class AddISBNToBookModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Book");
+        }
+    }
+}
---
+++ BookListRazor/Model/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        
+        
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
